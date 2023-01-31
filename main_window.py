3c2e7b18cec71a59316c87fec6928ad5b2737311
Ben Garneau <benjamin.garneau31@gmail.com>
2023-01-31 12:06:57 -0500

    update to window creations

--- main_window.py
+++ main_window.py
@@ -5,7 +5,7 @@ from PIL import Image, ImageTk
 import customtkinter
 from customtkinter import *
 import turtle
-from window_creation import WindowCreation
+from window_creation import PlanarWindowCreation, GloveWindowCreation
 
 customtkinter.set_appearance_mode("system")  # Modes: system (default), light, dark
 customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
@@ -15,6 +15,7 @@ CLOSE_LEFT = (-225, 0)
 CLOSE_RIGHT = (225, 0)
 now = time.ctime()
 print("Current Time:", now)
+
 
 # open passive control: its controls underneath it
 # open active (push button) control -- add planar control together, if active then the arduino will do work and python will just print it
@@ -28,12 +29,13 @@ class RootWindow:
     def __init__(self, master):
         # self.patientWindow = PatientWindow()  # opens patient window
         # self.arduino_data = PythonArduino()  # pull in arduino data object
-
+        self.sub_window = None
+        self.sub_window2 = None
         # left frame
         self.frame_1 = customtkinter.CTkFrame(master)
 
         user_label = customtkinter.CTkLabel(self.frame_1, text='User Number:')
-        user_label.grid(row=1, column=0,pady=20)
+        user_label.grid(row=1, column=0, pady=20)
         self.user_entry_var = customtkinter.StringVar()
         user_entry = customtkinter.CTkEntry(self.frame_1, textvariable=self.user_entry_var)
         user_entry.grid(row=1, column=1, padx=10)
@@ -44,8 +46,9 @@ class RootWindow:
         trial_entry = customtkinter.CTkEntry(self.frame_1, textvariable=self.trial_entry_var)
         trial_entry.grid(row=2, column=1, padx=10)
 
-        button1 = customtkinter.CTkButton(self.frame_1, text="Submit User/Trial Data", command=self.update_user_data)
-        button1.grid(row=3, column=0, columnspan=2, padx=10, pady=20)
+        submit_button = customtkinter.CTkButton(self.frame_1, text="Submit User/Trial Data",
+                                                command=self.update_user_data)
+        submit_button.grid(row=3, column=0, columnspan=2, padx=10, pady=20)
 
         collect_data_box = customtkinter.CTkCheckBox(self.frame_1, text="Record Data", command=self.record_data)
         collect_data_box.grid(row=4, column=0, columnspan=2, padx=50, pady=10)
@@ -53,19 +56,27 @@ class RootWindow:
         speed_label = customtkinter.CTkLabel(self.frame_1, text='Speed Setting:')
         speed_label.grid(row=5, column=0, columnspan=2, pady=(20, 0))
         self.speed_var = customtkinter.StringVar()
-        speed_slider = customtkinter.CTkComboBox(self.frame_1, variable=self.speed_var, values=['Slow', 'Fast'], state='normal', command=self.update_speed)
+        speed_slider = customtkinter.CTkComboBox(self.frame_1, variable=self.speed_var, values=['Slow', 'Fast'],
+                                                 state='normal', command=self.update_speed)
         speed_slider.grid(row=6, column=0, columnspan=2, padx=10, pady=20)
 
-        self.run_trial_but = customtkinter.CTkButton(self.frame_1, text="Run Trial", command=self.run_trial, fg_color="green")
+        self.run_trial_but = customtkinter.CTkButton(self.frame_1, text="Run Trial", command=self.run_trial,
+                                                     fg_color="green")
         self.run_trial_but.grid(row=7, column=0, columnspan=2, padx=10, pady=20)
 
         self.frame_1.grid(padx=40, pady=50, row=0, column=0)
 
-        # right frame
+        # second frame
+
         self.frame_2 = customtkinter.CTkFrame(master)
 
-        button1 = customtkinter.CTkButton(self.frame_2, text="Open Planar Window", command=self.open_planar_exo_window)
-        button1.grid(row=0, column=0, columnspan=2, padx=10, pady=20)
+        open_exo_but = customtkinter.CTkButton(self.frame_2, text="Open Planar Window",
+                                               command=self.open_planar_exo_window)
+        open_exo_but.grid(row=0, column=0, columnspan=1, padx=10, pady=20)
+
+        close_exo_but = customtkinter.CTkButton(self.frame_2, text="Close Planar Window",
+                                                command=self.close_planar_exo_window)
+        close_exo_but.grid(row=0, column=1, columnspan=1, padx=10, pady=20)
 
         close_left_but = customtkinter.CTkButton(self.frame_2, text="Go Close Left", command=self.go_close_left)
         close_left_but.grid(padx=50, pady=20, row=1, column=0)
@@ -85,8 +96,12 @@ class RootWindow:
         calibrate_but = customtkinter.CTkButton(self.frame_2, text="Calibrate", command=self.calibrate)
         calibrate_but.grid(padx=50, pady=20, row=5, column=0, columnspan=2)
 
+        self.run_trial_but = customtkinter.CTkButton(self.frame_2, text="Run Trial", command=self.run_trial,
+                                                     fg_color="green")
+        self.run_trial_but.grid(row=6, column=0, columnspan=1, padx=10, pady=20)
+
         stop_but = customtkinter.CTkButton(self.frame_2, text="Stop Trial", command=self.stop_trial, fg_color="red")
-        stop_but.grid(padx=50, pady=20, row=6, column=0, columnspan=2)
+        stop_but.grid(padx=50, pady=20, row=6, column=1, columnspan=1)
 
         self.frame_2.grid(padx=20, pady=50, row=0, column=1)
 
@@ -95,18 +110,36 @@ class RootWindow:
         self.frame_3 = customtkinter.CTkFrame(master)
 
         open_glove_but = customtkinter.CTkButton(self.frame_3, text="Open Glove Window", command=self.open_glove_window)
-        open_glove_but.grid(row=0, column=0, columnspan=2, padx=10, pady=20)
+        open_glove_but.grid(row=0, column=0, columnspan=1, padx=10, pady=10)
+
+        close_glove_but = customtkinter.CTkButton(self.frame_3, text="Close Glove Window", command=self.close_glove_window)
+        close_glove_but.grid(row=0, column=1, columnspan=1, padx=10, pady=10)
+
+        self.run_glove_trial = customtkinter.CTkButton(self.frame_3, text="Run Trial", command=self.run_trial, fg_color="green")
+        self.run_glove_trial.grid(row=2, column=0, columnspan=1, padx=10, pady=10)
+
+        stop_glove = customtkinter.CTkButton(self.frame_3, text="Stop Trial", command=self.stop_trial, fg_color="red")
+        stop_glove.grid(padx=10, pady=10, row=2, column=1, columnspan=1)
 
         self.frame_3.grid(padx=20, pady=50, row=0, column=2)
 
     # Root Window Functions
 
-    def open_planar_exo_window(self):  # toplevel window now created first when program run, so this function is depreciated
-        sub_window = WindowCreation(title="Planar Exoskeleton Window")
+    def open_planar_exo_window(self):
+        self.sub_window = PlanarWindowCreation()
+        print("Planar Window Created")
+
+    def close_planar_exo_window(self):
+        self.sub_window.close_window()
+        print("Planar Window Closed")
 
     def open_glove_window(self):
-        sub_window = WindowCreation(title="Exo Glove Window")
+        self.sub_window2 = GloveWindowCreation()
+        print("Glove Window Created")
 
+    def close_glove_window(self):
+        self.sub_window2.close_window()  # need the whole window to load and print out "created" statement before closing
+        print("Glove Window Closed")
 
     def update_user_data(self):
         # update csv file with user number and trial num
@@ -120,7 +153,7 @@ class RootWindow:
         print("User and Trial Data Collected")
 
     def record_data(self):
-        # update bool value to update "data collection" function that will take some type of data (eeg,emg)
+        # update bool value to update "data collection" function that will take some type of data
         pass
 
     def run_trial(self):
@@ -156,7 +189,7 @@ class RootWindow:
 
 if __name__ == "__main__":
     root = CTk()
-    root.geometry("%dx%d+%d+%d" % (1120, 500, 300, 300))
+    root.geometry("%dx%d+%d+%d" % (1280, 500, 300, 300))
     root.title("Exoskeleton Technician Controls")
     main_window = RootWindow(root)
     root.mainloop()
--- window_creation.py
+++ window_creation.py
@@ -5,30 +5,95 @@
 import customtkinter
 from customtkinter import *
 from tkinter import *
+import turtle
+from PIL import Image, ImageTk
 
 
-class WindowCreation():
+class PlanarWindowCreation(CTkToplevel):
 
-    def __init__(self, title):
-        self.top = customtkinter.CTkToplevel()
+    def __init__(self):
+        self.top = CTkToplevel()
         self.top.geometry("%dx%d+%d+%d" % (1050, 900, 0, 0))
-        self.top.title(title)
+        self.top.title("Exo Planar Window")
 
-        if title == "Planar Exoskeleton Window":
+        # Top Frame
+        w = 1050
+        h = 450
 
-            w = 1050
-            h = 450
+        self.frame_top = customtkinter.CTkFrame(self.top)
+        self.canvas = Canvas(self.frame_top, width=w, height=h, bg='gray')
+        self.canvas.grid(row=0, column=0)
+        self.frame_top.grid(row=0, column=0)
 
-            self.frame_top = customtkinter.CTkFrame(self.top)
-            self.canvas = Canvas(self.frame_top, width=w, height=h, bg='gray')
-            self.canvas.grid(row=0, column=0)
-            self.frame_top.grid(row=0, column=0)
+        # Bottom Frame
+        self.frame_bottom = customtkinter.CTkFrame(self.top)
 
-        elif title == "Exo Glove Window":
-            w = 500
-            h = 500
+        im = Image.open("images/please-wait2.png")
+        resized = im.resize((600, 400), Image.Resampling.LANCZOS)
+        ph = ImageTk.PhotoImage(resized)
 
-            self.frame_top = customtkinter.CTkFrame(self.top)
-            self.canvas = Canvas(self.frame_top, width=w, height=h, bg='gray')
-            self.canvas.grid(row=0, column=0)
-            self.frame_top.grid(row=0, column=0)
+        self.label = Label(self.frame_bottom, image=ph, width=600, height=400)
+        self.label.image = ph  # need to keep the reference of your image to avoid garbage collection
+        self.label.grid(row=1, column=0)
+        self.frame_bottom.grid(row=1, column=0, pady=20)
+
+        # Turtle Canvas and Turtle Objects
+        self.screen = turtle.TurtleScreen(self.canvas)
+        self.screen.bgcolor("cyan")
+        self.user_turtle = turtle.RawTurtle(self.screen, shape='turtle')
+        self.user_turtle.shapesize(stretch_len=2, stretch_wid=2)
+        self.user_turtle.color("green")
+        self.user_turtle.penup()
+
+        self.center_wall = turtle.RawTurtle(self.screen, shape="circle")
+        self.center_wall.hideturtle()
+        self.center_wall.shapesize(stretch_len=2, stretch_wid=2)
+        self.center_wall.color("light green")
+        self.center_wall.penup()
+
+        self.left_wall = turtle.RawTurtle(self.screen, shape="square")
+        self.left_wall.hideturtle()
+        self.left_wall.shapesize(stretch_len=1, stretch_wid=5)
+        self.left_wall.color("saddle brown")
+        self.left_wall.penup()
+        self.left_wall.setx(-450)
+
+        self.right_wall = turtle.RawTurtle(self.screen, shape="square")
+        self.right_wall.hideturtle()
+        self.right_wall.shapesize(stretch_len=1, stretch_wid=5)
+        self.right_wall.color("saddle brown")
+        self.right_wall.penup()
+        self.right_wall.setx(450)
+
+        self.left_wall_close = turtle.RawTurtle(self.screen, shape="square")
+        self.left_wall_close.hideturtle()
+        self.left_wall_close.shapesize(stretch_len=1, stretch_wid=5)
+        self.left_wall_close.color("saddle brown")
+        self.left_wall_close.penup()
+        self.left_wall_close.setx(-225)
+
+        self.right_wall_close = turtle.RawTurtle(self.screen, shape="square")
+        self.right_wall_close.hideturtle()
+        self.right_wall_close.shapesize(stretch_len=1, stretch_wid=5)
+        self.right_wall_close.color("saddle brown")
+        self.right_wall_close.penup()
+        self.right_wall_close.setx(225)
+
+    def close_window(self):
+        self.top.destroy()
+
+
+class GloveWindowCreation(CTkToplevel):
+
+    def __init__(self):
+        self.top2 = CTkToplevel()
+        self.top2.geometry("%dx%d+%d+%d" % (500, 500, 0, 0))
+        self.top2.title("Exo Glove Window")
+
+    def close_window(self):
+        self.top2.destroy()
+
+
+
+
+
